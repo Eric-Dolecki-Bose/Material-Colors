562f591f924d84ce37514eb603f9470aa3abcd0a
Eric Dolecki <eric_dolecki@bose.com>
2019-11-11 10:10:47 -0500

    Added hex color to the Sliver class and also it changes text color based on darkness of the background color.

--- MaterialColors/Sliver.swift
+++ MaterialColors/Sliver.swift
@@ -11,11 +11,48 @@ import UIKit
 class Sliver: UIView
 {
     // Custom initializer.
+    var label: UILabel!
+    
     init(frame: CGRect, background: UIColor) {
         super.init(frame: frame)
+        label = UILabel(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 7.0)
+        label.text = background.toRGBAString(uppercased: true)
+        label.textColor = textColor(bgColor: background)
+        self.addSubview(label)
         self.backgroundColor = background
     }
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 
+    // Returns black if the given background color is light or white if the given color is dark
+    func textColor(bgColor: UIColor) -> UIColor
+    {
+        var r: CGFloat = 0.0
+        var g: CGFloat = 0.0
+        var b: CGFloat = 0.0
+        var a: CGFloat = 0.0
+        var brightness: CGFloat = 0.0
+            bgColor.getRed(&r, green: &g, blue: &b, alpha: &a)
+        // algorithm from: http://www.w3.org/WAI/ER/WD-AERT/#color-contrast
+            brightness = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+        if (brightness < 0.5) {
+            return UIColor.white
+        } else {
+            return UIColor.black
+        }
+    }
+}
+
+extension UIColor {
+    func toRGBAString(uppercased: Bool = true) -> String {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        self.getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgba = [r, g, b, a].map { $0 * 255 }.reduce("", { $0 + String(format: "%02x", Int($1)) })
+        return uppercased ? rgba.uppercased() : rgba
+    }
 }
